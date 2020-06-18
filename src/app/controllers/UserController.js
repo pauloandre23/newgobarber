fdae41383f7eb1b9d3256d97d0198799f2bc6339
paulo meu filho <pauloandre232@gmail.com>
2020-06-18 11:54:18 -0300

    added jwt config. next step is auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,21 @@ import User from '../models/User'
 
  class UserController{
     async store(req,res){
-        const user = User.create(req.body)
-        return res.json(user)
+
+        const userExists = await User.findOne({where: {email: req.body.email}})
+        if (userExists){
+            return res.status(400).json({error: 'Usuário já está cadastrado'})
+        }
+
+
+
+        const {id, name, email, provider} = await User.create(req.body)
+        return res.json({
+            id,
+            name,
+            email, 
+            provider
+        })
     }
 }
 
