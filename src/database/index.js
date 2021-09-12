d97d0381b6d1c788d52108351ed29f947c6a8b3c
paulo meu filho <pauloandre232@gmail.com>
2021-09-12 20:43:38 -0300

    chama bb

--- src/database/index.js
+++ src/database/index.js
@@ -24,8 +24,8 @@ class Database{
     }
     mongo() {
         this.mongoConnection = mongoose.connect(
-            'mongodb://192.168.99.100:27017/gobarber', 
-            {useNewUrlParser: true, useFindAndModify: true}
+            'mongodb://localhost:27017/bootcampnodejs', 
+            {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true}
             );
         }
 }
