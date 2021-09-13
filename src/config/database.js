84701de85acd0a4662fec94c2f69442c899b393e
paulo meu filho <pauloandre232@gmail.com>
2021-09-12 21:03:30 -0300

    adds pass for docker postgres

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'bootcampdeploy',
   database: 'gobarber',
   define: {
       timestamps: true,
